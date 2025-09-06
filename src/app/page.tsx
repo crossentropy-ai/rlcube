fc3783eae016327a58ca66452bfae65051e42426
Yiwei Gong <imwithye@gmail.com>
2025-09-06 20:00:39 +0800

    add basic env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">Hello, RLCube!</main>;
+"use client";
+
+import { useControls } from "leva";
+import { Canvas } from "@react-three/fiber";
+import {
+  Center,
+} from "@react-three/drei";
+
+import { Env } from "../components/env";
+
+export default function App() {
+  return (
+    <Canvas shadows camera={{ position: [0, 0, 4.5], fov: 50 }}>
+      <group position={[0, -0.65, 0]}>
+        <Sphere />
+      </group>
+      <Env />
+    </Canvas>
+  );
+}
+
+function Sphere() {
+  const { roughness } = useControls({
+    roughness: { value: 1, min: 0, max: 1 },
+  });
+  return (
+    <Center top>
+      <mesh castShadow>
+        <sphereGeometry args={[0.75, 64, 64]} />
+        <meshStandardMaterial metalness={1} roughness={roughness} />
+      </mesh>
+    </Center>
+  );
 }
---
+++ src/components/env.tsx
@@ -0,0 +1,35 @@
+"ue client";
+
+import { useState, useTransition } from "react";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { PresetsType } from "@react-three/drei/helpers/environment-assets";
+import { useControls } from "leva";
+
+export const Env = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, startTransition] = useTransition();
+
+  const [preset, setPreset] = useState<PresetsType>("sunset");
+  useControls({
+    preset: {
+      value: preset,
+      options: ["sunset", "dawn", "forest"],
+      onChange: (value) => startTransition(() => setPreset(value)),
+    },
+  });
+  return (
+    <>
+      <OrbitControls
+        autoRotate
+        autoRotateSpeed={0.5}
+        enablePan={false}
+        enableZoom={true}
+        minDistance={4}
+        maxDistance={10}
+        minPolarAngle={Math.PI / 2.1}
+        maxPolarAngle={Math.PI / 2.1}
+      />
+      <Environment preset={preset} background blur={1} />
+    </>
+  );
+};
