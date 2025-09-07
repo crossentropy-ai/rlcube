7096d86069d0f825f70734418328cd64312843f3
Yiwei Gong <imwithye@gmail.com>
2025-09-07 18:13:33 +0800

    disable selection

--- src/components/ui.tsx
+++ src/components/ui.tsx
@@ -6,7 +6,7 @@ import { UITitle } from './ui-title';
 export const UI = () => {
   return (
     <div className="absolute inset-0">
-      <div className="w-full h-full p-6 flex flex-col justify-between">
+      <div className="w-full h-full p-6 flex flex-col justify-between select-none">
         <UITitle />
         <UIControls />
       </div>
